4e7d0c262ee44060091ec4915cd55cd5ed90a1ce
Galina Boteva <galka.boteva@gmail.com>
2022-11-10 18:44:32 +0200

    my solution of Word problem

---
+++ OOP/4_Polymorphism/word_mySolution/Initialization.java
@@ -0,0 +1,13 @@
+package word;
+
+public class Initialization {
+
+    public static CommandInterface buildCommandInterface(StringBuilder text){
+
+        CommandInterface command = new CommandImpl(text);
+
+        command.init();
+
+        return command;
+    }
+}
