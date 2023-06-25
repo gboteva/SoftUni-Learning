f22a167a0b7a727b94cb029ceda3d1770f9095f9
Galina Boteva <galka.boteva@gmail.com>
2023-06-25 14:44:14 +0300

    change folder

--- SPRING_DATA/SPRING_INTRO/springIntro/src/main/java/com/example/springintro/entities/AgeRestriction.java
+++
@@ -1,5 +0,0 @@
-package com.example.springintro.entities;
-
-public enum AgeRestriction {
-    MINOR, TEEN, ADULT
-}
--- SPRING_DATA/SPRING_INTRO/springIntro/src/main/java/com/example/springintro/entities/EditionType.java
+++
@@ -1,5 +0,0 @@
-package com.example.springintro.entities;
-
-public enum EditionType {
-    NORMAL, PROMO, GOLD
-}
